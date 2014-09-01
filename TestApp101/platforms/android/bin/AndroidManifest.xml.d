0c38eed002a283a5f402a345a6ea20bc3c6a88be
MetaSimian <metasimian@gmail.com>
2014-09-01 19:43:10 +0800

    From http://www.techrepublic.com/blog/software-engineer/create-cross-platform-apps-with-phonegap-and-jquery-mobile/

---
+++ TestApp101/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/jorshali/Projects/phonegap/sencha-example/platforms/android/bin/AndroidManifest.xml : \
+/Users/jorshali/Projects/phonegap/sencha-example/platforms/android/AndroidManifest.xml \
---
+++ TestApp101/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/jorshali/Projects/phonegap/sencha-example/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
+/Users/jorshali/Projects/phonegap/sencha-example/platforms/android/bin/HelloWorld.ap_ \
+/Users/jorshali/Projects/phonegap/sencha-example/platforms/android/bin/classes.dex \
+/Users/jorshali/Projects/phonegap/sencha-example/platforms/android/libs/cordova-3.2.0.jar \
